c9c3e0bdf1d44388f8b726cb3c60ebd31f2c65af
dogkim <ehrbs033@gmail.com>
2026-01-22 20:06:08 +0900

    main publish_dir fix

--- documents/.vuepress/config.mjs
+++ documents/.vuepress/config.mjs
@@ -1,6 +1,5 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
-// 분리한 파일에서 목록들을 불러옵니다.
 import * as CONST from './const.mjs'
 
 export default defineUserConfig({
