0735e32269af1d645dba01f4e7682b9ac3fea19b
dogkim <ehrbs033@gmail.com>
2026-02-06 17:42:41 +0900

    ㅁㅁ

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
-// import './custom.css'
+// 1. 컴포넌트 가져오기 (경로가 훨씬 깔끔해집니다)
+import Snow from './components/Snow.vue' 
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app }) {
+    // 2. 'Snow'라는 이름으로 전역 등록
+    app.component('Snow', Snow)
+  }
 }
--- docs/index.md
+++ docs/index.md
@@ -2,12 +2,8 @@
 layout: home
 title: My TIL
 ---
-# Welcome to My TIL
-
-<script setup>
-// 방금 만든 컴포넌트 불러오기 (경로 주의!)
-import Snow from './.vitepress/theme/components/Snow.vue'
-</script>
 
 <Snow />
+
+# Welcome to My TIL
 
